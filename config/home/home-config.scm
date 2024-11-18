172d849cd465a3f0871c0a36ef0c6a58fdf688a0
Svitax Erevos <svitaxiom@gmail.com>
2024-11-18 14:34:13 -0500

    add gnu-make package

--- config/home/home-config.scm
+++ config/home/home-config.scm
@@ -1,6 +1,7 @@
 (define-module (config home home-config)
   #:use-module (guix gexp)
   #:use-module (gnu)
+  #:use-module (gnu packages)
   #:use-module (gnu packages fonts)
   #:use-module (gnu packages emacs)
   #:use-module (gnu packages librewolf)
@@ -14,17 +15,19 @@
 ;; TODO font-iosevka 2.1.0
 (home-environment
  (packages (list git
-		 font-iosevka
-		 librewolf
-		 ;; NOTE make a home-onedrive-service-type
-		 ;; or use owncloud/nextcloud
-		 +onedrive
-		 ))
+                 gnu-make
+                 font-iosevka
+                 librewolf
+                 ;; NOTE make a home-onedrive-service-type
+                 ;; or use owncloud/nextcloud
+                 +onedrive
+
+                 ))
  (services (list
-	    (service home-emacs-config-service-type)
-	    (service home-dotfiles-service-type
-		     (home-dotfiles-configuration
-		      (directories '("../../files"))))
-	    (service home-bash-service-type
-		     (home-bash-configuration
-		      (environment-variables '(("EDITOR" . "emacs"))))))))
+            (service home-emacs-config-service-type)
+            (service home-dotfiles-service-type
+                     (home-dotfiles-configuration
+                      (directories '("../../files"))))
+            (service home-bash-service-type
+                     (home-bash-configuration
+                      (environment-variables '(("EDITOR" . "emacs"))))))))
