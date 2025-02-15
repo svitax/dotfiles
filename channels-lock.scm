21548dfb4f182ec1a770f565e69f21a62d532adc
Svitax Erevos <svitaxiom@gmail.com>
2025-02-15 04:26:22 -0500

    guix: update channels-lock

--- channels-lock.scm
+++ channels-lock.scm
@@ -3,7 +3,7 @@
         (url "https://git.savannah.gnu.org/git/guix.git")
         (branch "master")
         (commit
-          "4473f8ae902c2192cab6919363a9101ce9861e45")
+          "9fa107c774017e35d2b0f05c2032ba520b9c0218")
         (introduction
           (make-channel-introduction
             "9edb3f66fd807b096b48283debdcddccfea34bad"
