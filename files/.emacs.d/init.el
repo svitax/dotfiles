6433e80eefbec14ba2b6096d7a923b1292ee4a77
Svitax Erevos <svitaxiom@gmail.com>
2025-02-15 04:28:16 -0500

    emacs: adding a bunch of test things

--- files/.emacs.d/init.el
+++ files/.emacs.d/init.el
@@ -833,6 +833,11 @@ writeable."
   ;; visit. Turn on place saving globally.
   (save-place-mode))
 
+;;;;;;;;;;;;;;;;;;;
+;;;; bookmarks ;;;;
+
+;;;;;;;;;;;;;;;;;;;
+;;;; registers ;;;;
 
 ;;;;;;;;;;;;;;;;;;;;
 ;;;; minibuffer ;;;;
@@ -1342,6 +1347,37 @@ together."
   :init
   (eglot-tempel-mode t))
 
+;;;;;;;;;;;;;;
+;;;; capf ;;;;
+
+;; TODO document capf
+(use-package capf
+  :disabled t
+  :no-require
+  :config
+  (when (fboundp 'cape-capf-prefix-length)
+    (defalias '+capf-general (cape-capf-prefix-length
+                              (cape-capf-super
+                               #'cape-dabbrev ; `dabbrev-capf' errors
+                               #'cape-dict ; `ispell-complete-word' also?
+                               #'cape-keyword
+                               #'cape-line)
+                              1))
+    (defalias '+capf-file (cape-capf-prefix-length #'cape-file 1))
+    (defalias '+capf-template (cape-capf-prefix-length #'tempel-expand 1))
+
+    (defun +capf-add-capfs (&optional global)
+      "Add `completion-at-point' functions to GLOBAL hook if non-nil, else
+local."
+      (let ((local (not global)))
+        (add-hook 'completion-at-point-functions #'+capf-general -10 local)
+        (add-hook 'completion-at-point-functions #'+capf-template -10 local)
+        (add-hook 'completion-at-point-functions #'+capf-file -10 local)
+        (when local
+          ;; Do not run the global hook; everything is already added locally.
+          (delq t completion-at-point-functions)))))
+
+  (+capf-add-capfs t))
 
 ;;;;;;;;;;;;;;;;;
 ;;;; project ;;;;
@@ -2138,8 +2174,9 @@ buffer's window as well."
    ;; ("f" . ace-link-info)
    ;; :map woman-mode-map
    ;; ("f" . ace-link-woman)
-   :map eww-mode-map
-   ("f" . ace-link-eww)))
+   ;; :map eww-mode-map
+   ;; ("f" . ace-link-eww)
+   ))
 
 (use-package dogears
   :disabled t
@@ -2556,6 +2593,16 @@ When the region is active, comment its lines instead."
   (add-to-list 'eglot-stay-out-of 'flymake)
   (add-hook 'flymake-diagnostic-functions 'eglot-flymake-backend))
 
+;; (use-package eglot-booster
+;;   :disabled t
+;;   :config
+;;   (eglot-booster-mode))
+
+;;;;;;;;;;;;;
+;;;; dap ;;;;
+
+;; (use-package dape)
+
 ;;;;;;;;;;;;;;;;;
 ;;;; compile ;;;;
 
@@ -2762,6 +2809,18 @@ When the region is active, comment its lines instead."
   (define-fringe-bitmap 'git-gutter-fr:modified [#b11111000] nil nil '(center repeated))
   (define-fringe-bitmap 'git-gutter-fr:deleted [#b11111000] nil nil '(center repeated)))
 
+;;;;;;;;;;;;;;;;;;
+;;;; terminal ;;;;
+
+;;;;;;;;;;;;;;;;
+;;;; comint ;;;;
+
+;;;;;;;;;;;;;;
+;;;; repl ;;;;
+
+;;;;;;;;;;;;;;;;
+;;;; eshell ;;;;
+
 ;;;;;;;;;;;;;;;
 ;;;; elisp ;;;;
 
@@ -2809,9 +2868,10 @@ When the region is active, comment its lines instead."
   ;; "/home/evermind/.guix-home/profile/bin/node" ; node-executable
   ;; "/home/evermind/.guix-home/profile/bin/eslint" ; eslint-executable
   ;; "/home/evermind/.guix-home/profile/bin/dapDebugServer" ; vscode-js-debug-executable
-  :mode (("\\.jsx?\\'" . jtsx-jsx-mode)
-         ("\\.tsx\\'" . jtsx-tsx-mode)
-         ("\\.ts\\'" . jtsx-typescript-mode))
+  :init
+  (add-to-list 'auto-mode-alist '("\\.jsx?\\'" . jtsx-jsx-mode))
+  (add-to-list 'auto-mode-alist '("\\.tsx\\'" . jtsx-tsx-mode))
+  (add-to-list 'auto-mode-alist '("\\.ts\\'" . jtsx-typescript-mode))
   :config
   ;; TODO find a way to not have to bake in tsserver.path. maybe have typescript
   ;; package installed through a project guix.scm
@@ -2826,6 +2886,9 @@ When the region is active, comment its lines instead."
   ;; TODO remove all the apheleia-npx calls because they don't work with my setup
   (setf (alist-get 'prettier-javascript apheleia-formatters)
         '("prettier" "--stdin-filepath" filepath "--parser=babel-flow"
+          (apheleia-formatters-js-indent "--use-tabs" "--tab-width")))
+  (setf (alist-get 'prettier-json apheleia-formatters)
+        '("prettier" "--stdin-filepath" filepath "--parser=json"
           (apheleia-formatters-js-indent "--use-tabs" "--tab-width")))
 
   (setf (alist-get 'jtsx-jsx-mode apheleia-mode-alist)
@@ -3504,6 +3567,142 @@ continue, per `org-agenda-skip-function'."
    ("C-c 4" . org-srs-review-rate-again)))
 
 (use-package gnosis)
+
+;; TODO document org-repeat-todo
+(use-package org-repeat-todo
+  :no-require
+  :config
+  (defgroup repeat-todo nil
+    "Special repeaters for `org-mode' TODOs.")
+
+  (defvar repeat-todo-mode nil
+    "Non-nil if repeat-todo is enabled.")
+
+  (defun repeat-todo-mode-enable ()
+    "Enable repeat-todo mode."
+    (interactive)
+    (setq repeat-todo-mode t)
+    (add-hook 'org-after-todo-state-change 'repeat-todo--reschedule))
+
+  (defun repeat-todo-mode-disable ()
+    "Enable repeat-todo mode."
+    (interactive)
+    (setq repeat-todo-mode nil)
+    (remove-hook 'org-after-todo-state-change 'repeat-todo--reschedule))
+
+  (defun repeat-todo-mode-toggle ()
+    "Enable repeat-todo mode."
+    (interactive)
+    (if repeat-todo-mode
+        (repeat-todo-mode-disable)
+      (repeat-todo-mode-enable)))
+
+  (defcustom repeat-todo--property "REPEAT"
+    "Property name to indicate how to handle DONE event repeated weekly.
+
+One of:
+
+- \"weekdays\": Mon, Tues, Wed, Thurs, Fri, Sat, Sun
+
+- \"weekends\": Sat, Sun
+
+- One or more of the days of the week;
+
+  For example: (1) \"M\" or \"Mon\" \"Monday\"; (2) \"M T W\"
+
+- One or more of the days of the week as numbers; sunday=0 or 7, monday=1, etc:
+
+  For example: \"1 3 5\" for Mon, Wed, Fri "
+    :group 'repeat-todo
+    :type 'string)
+
+  (defun repeat-todo--parse-property (prop-value)
+    "Return repeating days for PROP-VALUE as list of day numbers (sun=0, mon=1, etc)."
+    (let ((case-fold-search t))
+      (cond
+       ((string-match "weekday\\(s\\)?" prop-value)
+        '(1 2 3 4 5))
+       ((string-match "weekend\\(s\\)?" prop-value)
+        '(6 7))
+       ((string-match "[0-9]+" (replace-regexp-in-string " " "" prop-value))
+        ;; We use 7's internally to represent Sunday. However, users can specify
+        ;; Sunday with either 0 or 7.
+        (cl-subst 7 0
+                  (mapcar
+                   'string-to-number (string-split prop-value " " 'omit-nulls))
+                  :test 'eq))
+       (t
+        (let ((repeat-days '())
+              (prop-values (string-split prop-value " " 'omit-nulls)))
+          (dolist (value prop-values)
+            (cond
+             ;; su, sun, sunday
+             ((string-match "^su\\(n\\(day\\)?\\)?" value)
+              (push 7 repeat-days))
+             ;; m, mon, monday
+             ((string-match "^m\\(on\\(day\\)?\\)?" value)
+              (push 1 repeat-days))
+             ;; Test for Thursday before Tuesday becasue we can mis-match on the "^T"
+             ;; r, thu, thur, thurs, thursday
+             ((or (string= "r" value)
+                  (string= "R" value)
+                  (string-match "^thu\\(r\\(s\\(day\\)?\\)?\\)?" value))
+              (push 4 repeat-days))
+             ;; t, tue, tues, tuesday
+             ((string-match "^t\\(ue\\(s\\(day\\)?\\)?\\)?" value)
+              (push 2 repeat-days))
+             ;; w, wed, wednesday
+             ((string-match "^w\\(ed\\(nesday\\)?\\)?" value)
+              (push 3 repeat-days))
+             ;; f, fri, friday
+             ((string-match "^f\\(ri\\(day\\)?\\)?" value)
+              (push 5 repeat-days))
+             ;; sa, sat, saturday
+             ((string-match "^sa\\(t\\(urday\\)?\\)?" value)
+              (push 6 repeat-days))))
+          (reverse repeat-days))))))
+
+  (defun repeat-todo--p (pom)
+    "Return non-nil if the heading at POM is configured to repeat on weekdays."
+    (let ((valid-repeaters '("++1d" ".+1d")))
+      (and (-contains-p valid-repeaters (org-get-repeat))
+           (org-entry-get pom repeat-todo--property))))
+
+  (defun repeat-todo--next-scheduled-time (current-scheduled-time weekdays)
+    "Return the next valid, by WEEKDAYS, time after CURRENT-SCHEDULED-TIME.
+
+WEEKDAYS: See `repeat-todo--weekdays'."
+    (when weekdays
+      (let ((new-scheduled-time
+             (time-add current-scheduled-time (days-to-time 1))))
+        (while (not
+                (member
+                 (string-to-number
+                  (format-time-string "%u" new-scheduled-time))
+                 weekdays))
+          (setq new-scheduled-time
+                (time-add new-scheduled-time (days-to-time 1))))
+        new-scheduled-time)))
+
+  (defun repeat-todo--reschedule (point-or-marker)
+    "Reschedule heading at POINT-OR-MARKER to the next appropriate weekday."
+    (when (and repeat-todo-mode
+               (org-entry-is-done-p)
+               (repeat-todo--p point-or-marker))
+      (org-schedule
+       nil
+       (string-replace
+        " 00:00" ""
+        (format-time-string
+         "%F %H:%M"
+         ;; Schedule to the day before the next schedule time because
+         ;; it'll get moved forward one day past when we schedule it
+         (time-subtract
+          (repeat-todo--next-scheduled-time
+           (org-get-scheduled-time point-or-marker)
+           (repeat-todo--parse-property
+            (or (org-entry-get point-or-marker repeat-todo--property) "")))
+          (days-to-time 1))))))))
 
 ;;;;;;;;;;;;;;;;;;;;;;
 ;;;; bibliography ;;;;
